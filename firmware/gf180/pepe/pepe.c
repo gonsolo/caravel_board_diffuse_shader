ed0cb9d9309ae701b2bcdf172aab663b20d67837
Gon Solo <gonsolo@gmail.com>
2024-09-25 11:47:16 +0200

    Size matters!
    If the firmware is too big, flashing fails.

--- firmware/gf180/pepe/pepe.c
+++ firmware/gf180/pepe/pepe.c
@@ -12,6 +12,7 @@
 
 #include <defs.h>
 
+#if 0
 void dummyDelay(int num)
 {
         for (int i=0;i < num;i++){
@@ -194,85 +195,156 @@ void configure_io()
                     ;
 }
 
-void delay(const int d)
+#define WRITE 28
+#define CHANNEL_SELECT0 27
+#define CHANNEL_SELECT1 26
+#define BYTE_SELECT0 25
+#define BYTE_SELECT1 24
+#define INPUT7 23
+#define INPUT6 INPUT7 - 1
+#define INPUT5 INPUT7 - 2
+#define INPUT4 INPUT7 - 3
+#define INPUT3 INPUT7 - 4
+#define INPUT2 INPUT7 - 5
+#define INPUT1 INPUT7 - 6
+#define INPUT0 INPUT7 - 7
+#define OUTPUT7 12
+#define OUTPUT0 OUTPUT7 - 7
+
+void configure_gpio()
 {
-        /* Configure timer for a single-shot countdown */
+        GPIOs_configure(WRITE, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(CHANNEL_SELECT0, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(CHANNEL_SELECT1, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(BYTE_SELECT0, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(BYTE_SELECT1, GPIO_MODE_USER_STD_INPUT_NOPULL);
+
+        GPIOs_configure(INPUT7, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT6, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT5, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT4, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT3, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT2, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT1, GPIO_MODE_USER_STD_INPUT_NOPULL);
+        GPIOs_configure(INPUT0, GPIO_MODE_USER_STD_INPUT_NOPULL);
+
+        for (int i = OUTPUT0; i <= OUTPUT7; i++) {
+                GPIOs_configure(i, GPIO_MODE_USER_STD_OUTPUT);
+        }
+}
+#endif
+
+void blink_delay(const int d)
+{
 	reg_timer0_config = 0;
 	reg_timer0_data = d;
         reg_timer0_config = 1;
 
-        // Loop, waiting for value to reach zero
-        reg_timer0_update = 1;  // latch current value
+        reg_timer0_update = 1;
         while (reg_timer0_value > 0) {
                 reg_timer0_update = 1;
         }
 }
 
+
+void blink_configure_io()
+{
+    reg_mprj_io_0 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+
+    // Changing configuration for IO[1-4] will interfere with programming flash. if you change them,
+    // You may need to hold reset while powering up the board and initiating flash to keep the process
+    // configuring these IO from their default values.
+
+    reg_mprj_io_1 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_2 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+    reg_mprj_io_3 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+    reg_mprj_io_4 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+
+    reg_mprj_io_5 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;     // UART Rx
+    reg_mprj_io_6 = GPIO_MODE_MGMT_STD_OUTPUT;           // UART Tx
+    reg_mprj_io_7 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_8 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_9 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_10 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_11 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_12 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_13 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_14 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_15 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+    reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_32 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_33 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_34 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_35 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_36 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+    // Initiate the serial transfer to configure IO
+    reg_mprj_xfer = 1;
+    while (reg_mprj_xfer == 1);
+}
+
+
+void blink_main()
+{
+	int i, j, k;
+
+        reg_gpio_mode1 = 1;
+        reg_gpio_mode0 = 0;
+        reg_gpio_ien = 1;
+        reg_gpio_oeb = 0;
+
+        blink_configure_io();
+
+	reg_la0_oenb = reg_la0_iena = 0x00000000;    // [31:0]
+	reg_la1_oenb = reg_la1_iena = 0x00000000;    // [63:32]
+	reg_la2_oenb = reg_la2_iena = 0x00000000;    // [95:64]
+	reg_la3_oenb = reg_la3_iena = 0x00000000;    // [127:96]
+
+	while (1) {
+                reg_gpio_out = 1; // OFF
+                reg_mprj_datah = 0x0000002a;
+                reg_mprj_datal = 0xaaaaaaaa;
+
+		blink_delay(4000000);
+
+                reg_gpio_out = 0;  // ON
+                reg_mprj_datah = 0x00000015;
+                reg_mprj_datal = 0x55555555;
+
+		blink_delay(4000000);
+
+        }
+}
+
 void main()
 {
-        //int i, j, k;
+        blink_main();
 
-#if 0
-        ManagmentGpio_outputEnable();
-        ManagmentGpio_write(0);
-        enableHkSpi(0);
+        //ManagmentGpio_outputEnable();
+        //ManagmentGpio_write(0);
+        //enableHkSpi(0);
 
-        for (int i = 5; i < 13; i++) {
-                GPIOs_configure(i, GPIO_MODE_USER_STD_OUTPUT);
-        }
-        for (int i = 13; i < 21; i++) {
-                GPIOs_configure(i, GPIO_MODE_MGMT_STD_INPUT_PULLDOWN);
-        }
-        for (int i = 21; i < 29; i++) {
-                GPIOs_configure(i, GPIO_MODE_MGMT_STD_INPUT_PULLDOWN);
-        }
+        //configure_gpio();
 
-        GPIOs_loadConfigs();
-        ManagmentGpio_write(1);
-#endif
-
-        //reg_gpio_mode1 = 1;
-        //reg_gpio_mode0 = 0;
-        //reg_gpio_ien = 1;
-        //reg_gpio_oeb = 1;
-
-        //configure_io();
-
-        // reg_uart_enable = 1;
-
-        // Configure All LA probes as inputs to the cpu
-	//reg_la0_oenb = reg_la0_iena = 0x00000000;    // [31:0]
-	//reg_la1_oenb = reg_la1_iena = 0x00000000;    // [63:32]
-	//reg_la2_oenb = reg_la2_iena = 0x00000000;    // [95:64]
-	//reg_la3_oenb = reg_la3_iena = 0x00000000;    // [127:96]
-
-	// write data to la output
-        // reg_la0_data = 0x00;
-        // reg_la1_data = 0x00;
-        // reg_la2_data = 0x00;
-        // reg_la3_data = 0x00;
-
-        // read data from la input
-        //	data0 = reg_la0_data;
-        //	data1 = reg_la1_data;
-        //	data2 = reg_la2_data;
-        //	data3 = reg_la3_data;
-
-        // print("Hello World !!");
-
-	//while (1) {
-
-        //        reg_gpio_out = 1; // OFF
-        //        reg_mprj_datah = 0x0000002a;
-        //        reg_mprj_datal = 0xaaaaaaaa;
-
-	//	delay(4000000);
-
-        //        reg_gpio_out = 0;  // ON
-        //        reg_mprj_datah = 0x00000015;
-        //        reg_mprj_datal = 0x55555555;
-
-	//	delay(4000000);
-        //}
+        //GPIOs_loadConfigs();
+        //ManagmentGpio_write(1);
+        //enableHkSpi(1);
 }
 
